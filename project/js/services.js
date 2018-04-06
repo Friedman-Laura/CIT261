24b3221d5110204857dd4848cf2b4e32ded07523
Laura Friedman <fri16006@byui.edu>
2018-04-05 19:23:01 -0500

    Final Version
    Final Project for CIT261 - BYU-Idaho - Laura Friedman

--- project/js/services.js
+++ project/js/services.js
@@ -17,6 +17,7 @@
         showServices(mgServices);
     }    
     
+    // Build the Services header
     function populateHeader(jsonObj) {
       var myTitle = document.createElement('blockquote');
       myTitle.textContent = jsonObj['sectionTitle'];
@@ -25,13 +26,18 @@
       mySubtitle.textContent = 'Available: ' + jsonObj['available'] + ' // Founded: ' + jsonObj['founded'];
       header.appendChild(mySubtitle);
     }
-        
+
+    // Build the Services section
     function showServices(jsonObj) {
       var services = jsonObj['services'];
       var svcRows = services.length;
+
+      // Determine the number of rows to display
       var svcRows = Math.ceil(svcRows / 3);
+        
       var keepGoing = true;
       var currColCount = 0;
+         
       while (keepGoing) {
             var myRow = document.createElement('div');
             myRow.className = 'row';
@@ -44,18 +50,35 @@
                     var myImage = document.createElement('img');
                     var myH6 = document.createElement('h6');
                     var myPara2 = document.createElement('p');
+                    
+                    var myTransformImage = document.createElement('div');
+                    var myTransformSubtitle = document.createElement('div');
 
+                    // Set the div to 30% of the screen width
                     myDiv.className = 'col-30';
+                    
                     mySection.className = 'services-container';
                     myH5.textContent = services[i].name;
+                    
+                    // Build the image to transform and display a service image caption on hover
+                    myTransformImage.className = 'services-image';
+                    myTransformSubtitle.className = 'services-image-caption';
+                    
                     myImage.src = services[i].svcImage;
                     myImage.textContent = services[i].svcImage;
+                                        
                     myH6.textContent = services[i].subTitle;
-                    myPara2.textContent = services[i].description;
+                    myTransformSubtitle.appendChild(myH6);
+                    
+                    myTransformImage.appendChild(myImage);
+                    myTransformImage.appendChild(myTransformSubtitle);                    
 
+                    // Display the Service description
+                    myPara2.textContent = services[i].description;
+                    
+                    // Append all the information for the Service to the correct elements
                     mySection.appendChild(myH5);
-                    mySection.appendChild(myImage);
-                    mySection.appendChild(myH6);
+                    mySection.appendChild(myTransformImage);
                     mySection.appendChild(myPara2);
                     myDiv.appendChild(mySection);
                     myRow.appendChild(myDiv);
